b765821326e45654ff788a81ea020629f54a369a
nedelweiss <edelweissnull@gmail.com>
2023-06-22 20:41:13 +0300

    Replaced mail library from angus to javax

--- src/main/java/com/github/ChuprinaVlad/Main.java
+++ src/main/java/com/github/ChuprinaVlad/Main.java
@@ -1,28 +1,37 @@
 package com.github.ChuprinaVlad;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.*;
-
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws MessagingException, IOException {
 
+    public static void main(String[] args) throws MessagingException, IOException {
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.host", "smtp.mailtrap.io");
-        prop.put("mail.smtp.port", "25");
+        prop.put("mail.smtp.port", 2525);
         prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+        prop.put("mail.smtp.starttls.enable", false);
+        prop.put("mail.smtp.EnableSSL.enable", false);
 
-        Session session = Session.getInstance(prop, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return super.getPasswordAuthentication();
-            }
-        });
+        Session session = Session.getDefaultInstance(prop,
+            new javax.mail.Authenticator(){
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(
+                        "testvlad.123456@gmail.com", "ko100lom");
+                }
+            });
 
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress("testvlad.123456@gmail.com"));

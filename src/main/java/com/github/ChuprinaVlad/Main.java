245461ec8e5c85943ee034bcea8b39a6529ead3f
ChuprinaVlad <Chuprinavlad@ukr.net>
2023-06-23 22:07:25 +0300

    Replaced mail library from angus to javax and corected PasswordAuthentication

--- src/main/java/com/github/ChuprinaVlad/Main.java
+++ src/main/java/com/github/ChuprinaVlad/Main.java
@@ -1,8 +1,11 @@
 package com.github.ChuprinaVlad;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.*;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
@@ -10,17 +13,21 @@ import java.util.Properties;
 public class Main {
     public static void main(String[] args) throws MessagingException, IOException {
 
+        final String  username = "testvlad.123456@gmail.com";
+        final String password = "ko100lom";
+
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", true);
         prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.mailtrap.io");
-        prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+//        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
-        Session session = Session.getInstance(prop, new Authenticator() {
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return super.getPasswordAuthentication();
+                return new PasswordAuthentication(username, password);
             }
         });
 

9b8153b5f5c22a5fd83d20be1c99554988c830bd
ChuprinaVlad <Chuprinavlad@ukr.net>
2023-06-23 23:45:21 +0300

    add setting properties, corrected username password

--- src/main/java/com/github/ChuprinaVlad/Main.java
+++ src/main/java/com/github/ChuprinaVlad/Main.java
@@ -14,14 +14,17 @@ public class Main {
     public static void main(String[] args) throws MessagingException, IOException {
 
         final String  username = "testvlad.123456@gmail.com";
-        final String password = "ko100lom";
+        final String password = "bxohylduhunadzbl";
 
         Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "465");
-//        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.starttls.required", "true");
+        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
 

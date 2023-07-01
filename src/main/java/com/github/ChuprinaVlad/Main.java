d233d7a7abe52d718311cd4ae3a99ce89a44e444
ChuprinaVlad <Chuprinavlad@ukr.net>
2023-07-01 20:06:59 +0300

    corrected messages to different addresses

--- src/main/java/com/github/ChuprinaVlad/Main.java
+++ src/main/java/com/github/ChuprinaVlad/Main.java
@@ -16,6 +16,8 @@ public class Main {
 
         final String  username = "testvlad.123456@gmail.com";
         final String password = "bxohylduhunadzbl";
+        final String [] emails = new String[] {"kolegran@gmail.com", "kolegran+1@gmail.com", "kolegran+2@gmail.com"};
+        String email, msg = null;
 
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", "true");
@@ -35,32 +37,50 @@ public class Main {
             }
         });
 
+
+
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(username));
-        message.setRecipients(
-                Message.RecipientType.TO, InternetAddress.parse("kolegran@gmail.com, kolegran+1@gmail.com"));
-        message.setSubject("Mail Subject");
 
-        String msg = "This is my second email using JavaMailer";
+        for (int i = 0; emails.length > i; i++) {
+            email = emails[i];
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject("Mail Subject");
 
-        MimeBodyPart mimeBodyPart = new MimeBodyPart();
-        mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
+        switch (email) {
+            case "kolegran@gmail.com":
+                msg = "Hi John Doe, this is my first email using JavaMailer";
+                break;
+            case "kolegran+1@gmail.com":
+                msg = "Hi Sam Baker, this is my first email using JavaMailer";
+                break;
+            case "kolegran+2@gmail.com":
+                msg = "Hi Alex Rider, this is my first email using JavaMailer";
+                break;
+            default:
+                System.out.println("Mail entered incorrectly");
+                break;
+        }
+                MimeBodyPart mimeBodyPart = new MimeBodyPart();
+                mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
 
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(mimeBodyPart);
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
 
 
-        MimeBodyPart attachmentBodyPart = new MimeBodyPart();
-        var res  = Main.class.getClassLoader().getResource("smile.jpg");
+            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+            var res = Main.class.getClassLoader().getResource("smile.jpg");
 
-        if (res != null) {
-            var file = new File(res.toURI());
-            attachmentBodyPart.attachFile(file);
-            multipart.addBodyPart(attachmentBodyPart);
+            if (res != null) {
+                var file = new File(res.toURI());
+                attachmentBodyPart.attachFile(file);
+                multipart.addBodyPart(attachmentBodyPart);
 
-            message.setContent(multipart);
+                message.setContent(multipart);
 
-            Transport.send(message);
+                Transport.send(message);
+            }
         }
 
     }
